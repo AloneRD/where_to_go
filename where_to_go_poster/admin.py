c04188af4c371b300a7747005fde965a25cdd4c2
Artur <artur_goncharov_97@mail.ru>
2022-06-04 17:33:53 +0300

    refactor:Add review image in AdminPlace

--- where_to_go_poster/admin.py
+++ where_to_go_poster/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from where_to_go_poster.models import Places
 from where_to_go_poster.models import ImagesPlaces
+from django.utils.html import format_html
 
 class ImagePlaceInline(admin.TabularInline):
     model = ImagesPlaces
+    readonly_fields = ("img_show",)
+
+
+    def img_show(self,instance):
+        return format_html('<img src="{}" width=200px  height=220px style="object-fit:contain"> ',instance.img.url)
+
 
 @admin.register(Places)
 class AdminPlace(admin.ModelAdmin):
     inlines = [ImagePlaceInline]
+     
 
 admin.site.register(ImagesPlaces)
