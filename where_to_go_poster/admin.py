1a87a4403334bf4cd0e5613d55396c958de72592
Artur <artur_goncharov_97@mail.ru>
2022-06-04 16:23:42 +0300

    refactor: Add AdminInline Images

--- where_to_go_poster/admin.py
+++ where_to_go_poster/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from where_to_go_poster.models import Places
 from where_to_go_poster.models import ImagesPlaces
 
-admin.site.register(Places)
+class ImagePlaceInline(admin.TabularInline):
+    model = ImagesPlaces
+
+@admin.register(Places)
+class AdminPlace(admin.ModelAdmin):
+    inlines = [ImagePlaceInline]
+
 admin.site.register(ImagesPlaces)
