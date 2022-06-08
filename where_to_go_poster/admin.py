a5298ad237948123064ae09fdd374c30d761f214
Artur <artur_goncharov_97@mail.ru>
2022-06-08 23:15:25 +0300

    refactor: Code formatting

--- where_to_go_poster/admin.py
+++ where_to_go_poster/admin.py
@@ -5,19 +5,19 @@ from django.utils.html import format_html
 from adminsortable2.admin import SortableInlineAdminMixin
 from adminsortable2.admin import SortableAdminMixin
 
-class ImagePlaceInline(SortableInlineAdminMixin,admin.TabularInline):
+
+class ImagePlaceInline(SortableInlineAdminMixin, admin.TabularInline):
     model = ImagesPlaces
     extra = 2
     readonly_fields = ("img_show",)
 
-
-    def img_show(self,instance):
-        return format_html('<img src="{}" width=200px  height=220px style="object-fit:contain"> ',instance.img.url)
+    def img_show(self, instance):
+        return format_html('<img src="{}" width=200px  height=220px style="object-fit:contain"> ', instance.img.url)
 
 
 @admin.register(Places)
-class AdminPlace(SortableAdminMixin,admin.ModelAdmin):
+class AdminPlace(SortableAdminMixin, admin.ModelAdmin):
     inlines = [ImagePlaceInline]
-     
+
 
 admin.site.register(ImagesPlaces)
--- where_to_go_poster/management/commands/upload_data.py
+++ where_to_go_poster/management/commands/upload_data.py
@@ -1,26 +1,27 @@
+import re
 from io import BytesIO
 import requests
-import re
 from django.core.management.base import BaseCommand
 from where_to_go_poster.models import Places
 from where_to_go_poster.models import ImagesPlaces
+
 
 class Command(BaseCommand):
     help = 'Add places in DataBase'
 
     def add_arguments(self, parser):
-        parser.add_argument('link', nargs='+', type=str,help='Link to data file')
+        parser.add_argument('link', nargs='+', type=str, help='Link to data file')
 
     def handle(self, *args, **options):
         data_place = requests.get(fr"{options['link'][0]}").json()
-        place,created = Places.objects.get_or_create(
+        place, created = Places.objects.get_or_create(
             title=data_place['title'],
-            description_short = data_place['description_short'],
-            description_long = data_place['description_long'],
-            coordinates =  data_place['coordinates']    
+            description_short=data_place['description_short'],
+            description_long=data_place['description_long'],
+            coordinates=data_place['coordinates']
         )
         print(place)
-        images = [{"content":BytesIO(requests.get(img).content),"name":re.findall(r"(.*)\/(.*).jpg|JPG|Jpg|jpeg|JPEG", img)[0][1]} for img in data_place['imgs']]
+        images = [{"content": BytesIO(requests.get(img).content), "name": re.findall(r"(.*)\/(.*).jpg|JPG|Jpg|jpeg|JPEG", img)[0][1]} for img in data_place['imgs']]
         for image in images:
-            new_place = ImagesPlaces(place = place)
-            new_place.img.save(f"{image['name']}.img",image['content'],save=True)
+            new_place = ImagesPlaces(place=place)
+            new_place.img.save(f"{image['name']}.img", image['content'], save=True)
